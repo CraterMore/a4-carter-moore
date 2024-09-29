327632fb12efdaf6a1394c6cf7fd9dbdbd130f10
CraterMore <ctm6704@yahoo.com>
2024-09-29 11:35:44 -0400

    adding github strategry proxy to fix oauth crash

--- auth.js
+++ auth.js
@@ -6,7 +6,8 @@ dotenv.config();
 passport.use(new strategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        callbackURL: `${process.env.VITE_REACT_APP_SERVER_API_URL}/auth/github/callback`
+        callbackURL: `${process.env.VITE_REACT_APP_SERVER_API_URL}/auth/github/callback`,
+        proxy: true
     },
     function(accessToken, refreshToken, profile, done) {
         return done(null, profile);
